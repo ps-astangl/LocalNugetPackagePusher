28558ae2ce16312499828fbd934f41c6b3ad7fc9
AJStangl <astangl@leaporbit.com>
2022-01-14 13:23:32 -0500

    only really needs one line

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 import sys
-
 from NugetFeedHelper import NugetFeedHelper
 
-
-def main():
-    config = sys.argv[1]
-    NugetFeedHelper().run(config)
-
-
 if __name__ == '__main__':
-    main()
+    NugetFeedHelper().run(sys.argv[1])
